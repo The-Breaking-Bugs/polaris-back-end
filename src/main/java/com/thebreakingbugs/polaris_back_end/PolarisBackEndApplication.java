b1cab18d13e6e5195cf3caf5bb19518044b156f3
Gabriel Lucas <gabriellucaslc10@gmail.com>
2025-11-29 17:57:17 -0300

    fix: HealthControllerTest and configuration file for Mongo

--- src/main/java/com/thebreakingbugs/polaris_back_end/PolarisBackEndApplication.java
+++ src/main/java/com/thebreakingbugs/polaris_back_end/PolarisBackEndApplication.java
@@ -2,10 +2,9 @@ package com.thebreakingbugs.polaris_back_end;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.mongodb.config.EnableMongoAuditing;
 
 @SpringBootApplication
-@EnableMongoAuditing
+
 public class PolarisBackEndApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/com/thebreakingbugs/polaris_back_end/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package com.thebreakingbugs.polaris_back_end.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+
+}
