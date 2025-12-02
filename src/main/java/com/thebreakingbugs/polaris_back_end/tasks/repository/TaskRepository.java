ba84a0034eb60b74366d6cb87b2bca10cb957ab4
Danilomartinsdeoliveira <danilooliveira.161103@gmail.com>
2025-12-02 11:24:34 -0300

    fix: remove blank line and add newline at end of TaskRepository.java
    Added a newline at the end of the file for consistency.

--- src/main/java/com/thebreakingbugs/polaris_back_end/tasks/repository/TaskRepository.java
+++ src/main/java/com/thebreakingbugs/polaris_back_end/tasks/repository/TaskRepository.java
@@ -8,7 +8,6 @@ import java.util.List;
 public interface TaskRepository extends MongoRepository<Task, String> {
     
     List<Task> findByOwnerIdAndStatusOrderByDueDateAsc(String ownerId, TaskStatus status);
-
     
     List<Task> findByOwnerIdAndStatusOrderByDueDateDesc(String ownerId, TaskStatus status);
 }
