f6583515ac35e118c4fb52326f46635d7cf5f0ac
Gabriel-Paes <paessouzagabriel@gmail.com>
2025-12-02 21:59:31 -0300

    feat: TBB-28 implement NoteService methods for retrieving, updating, and deleting notes, and add search functionality

--- src/main/java/com/thebreakingbugs/polaris_back_end/notes/service/NoteService.java
+++ src/main/java/com/thebreakingbugs/polaris_back_end/notes/service/NoteService.java
@@ -1,8 +1,11 @@
 package com.thebreakingbugs.polaris_back_end.notes.service;
 
+import com.thebreakingbugs.polaris_back_end.notes.dto.UpdateNoteDTO;
 import com.thebreakingbugs.polaris_back_end.notes.model.Note;
 import com.thebreakingbugs.polaris_back_end.notes.repository.NoteRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class NoteService {
@@ -24,5 +27,36 @@ public class NoteService {
         }
 
         return noteRepository.save(note);
+    }
+
+    public List<Note> findAllByModuleId(String moduleId, String ownerId) {
+        return noteRepository.findAllByModuleIdAndOwnerId(moduleId, ownerId);
+    }
+
+    public Note getDetails(String noteId, String moduleId, String ownerId) {
+        return noteRepository.findByIdAndModuleIdAndOwnerId(noteId, moduleId, ownerId)
+                .orElseThrow(() -> new RuntimeException("Note not found"));
+    }
+
+    public Note update(String noteId, String moduleId, UpdateNoteDTO changes, String ownerId) {
+        Note note = getDetails(noteId, moduleId, ownerId);
+
+        if (changes.title() != null) {
+            note.setTitle(changes.title());
+        }
+        if (changes.content() != null) {
+            note.setContent(changes.content());
+        }
+
+        return noteRepository.save(note);
+    }
+
+    public void delete(String noteId, String moduleId, String ownerId) {
+        Note note = getDetails(noteId, moduleId, ownerId);
+        noteRepository.delete(note);
+    }
+
+    public List<Note> search(String moduleId, String searchTerm, String ownerId) {
+        return noteRepository.searchByTermInModule(moduleId, searchTerm, ownerId);
     }
 }
